2647aa23dee46bc7d896b5a9e604b41a2e03ee95
HeroBeater <japan-alex@hotmail.fr>
2018-10-10 20:13:52 +0200

    add String to Firestore retrieve String to Firestore
    (don't forget to change rules of Firestore)

--- app/src/main/java/com/example/alex/worldoffoodrecipes/myRecipesActivity.java
+++ app/src/main/java/com/example/alex/worldoffoodrecipes/myRecipesActivity.java
@@ -1,13 +1,83 @@
 package com.example.alex.worldoffoodrecipes;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class myRecipesActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    ListView listView;
+    private List<String> namesList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recipes);
+
+        button = findViewById(R.id.btn);
+        editText = (EditText) findViewById(R.id.edittext);
+        listView = (ListView) findViewById(R.id.listView);
+
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //add Data
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, String> map = new HashMap<>();
+                map.put("name", editText.getText().toString());
+                db.collection("Recipe Name").document().set(map)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Data saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "Data NOT saved", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        // retrieve Data
+
+        db.collection("Recipe Name").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                namesList.clear();
+                for (DocumentSnapshot snapshot : documentSnapshots){
+                    namesList.add(snapshot.getString("name"));
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_selectable_list_item,namesList);
+                adapter.notifyDataSetChanged();
+                listView.setAdapter(adapter);
+            }
+        });
+
     }
 }
